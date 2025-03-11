dbf50739d409e9b2e53ad7919ea6f7d90ed274ec
Rodrigo Slongo <rodrigo.slongotur@hotmail.com>
2025-03-11 17:56:09 -0300

    docs: Muda nome das variavies metodos e documenta parte do codigo

--- src/main/java/probability/central/ArithmeticMean.java
+++ src/main/java/probability/central/ArithmeticMean.java
@@ -6,25 +6,25 @@ import java.util.List;
  *
  * @author Rodrigo Miotto Slongo (Slongo11)
  */
-public class arithmeticMean {
+public class ArithmeticMean {
 	/**
 	 * Método que calcula a {@code Média Aritimética} com um conjunto de {@link Number}.
 	 * @param conjuntoNumeros a ser calculado a {@code Média Aritimética}.
 	 * @return a {@code Média Aritimética} referente ao conjunto de {@link Number}.
 	 */
-	public static double calculaMediaAritimetica(List<Number> conjuntoNumeros) {
+	public static double arithmeticMean(List<Number> conjuntoNumeros) {
 		if (conjuntoNumeros.isEmpty()) {
 			return 0.0;
 		}
-		double soma = 0;
-		int quantidadeDeElementos = conjuntoNumeros.size();
-		for (int i = 0; i < quantidadeDeElementos; i++) {
+		Double soma = 0.0;
+		int elementsQuantity = conjuntoNumeros.size();
+		for (int i = 0; i < elementsQuantity; i++) {
 			// Realiza a soma de todos os elementos da lista/conjunto de dados.
 			soma += conjuntoNumeros.get(i).doubleValue();
 		}
 
-		double mediaAritimetica = soma / quantidadeDeElementos;
+		Double arithmeticMean = soma / elementsQuantity;
 
-		return mediaAritimetica;
+		return arithmeticMean;
 	}
 }
