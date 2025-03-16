56483559e275c52adcd4cb9ea1ed5fbcba5a7065
Lucas da Paz <xlucaspx@gmail.com>
2025-03-15 23:07:48 -0300

    fix: separa cálculo da variância entre amostral e populacional

--- readme.md
+++ readme.md
@@ -1,28 +1,29 @@
 # Probabilidade e Estatística - Trabalho I
 
-Linguagem de Programação: Java (21);
-Alunos:
-- [Bernardo Tarnowski Dallarosa](https://github.com/bernardotdallarosa);
-- [Lucas da Paz Olivera](https://github.com/xLucaspx);
-- [Nicolas Dutra Tondo](https://github.com/NicolasTondo);
-- Pedro Gonzatti Chiella;
-- [Rodrigo Miotto Slongo](https://github.com/Slongo11).
-
-[Trello](https://trello.com/b/tifTF5Vp).
+- Linguagem de Programação: Java (21);
+- [Trello](https://trello.com/b/tifTF5Vp);
+- Alunos:
+	- [Bernardo Tarnowski Dallarosa](https://github.com/bernardotdallarosa);
+	- [Lucas da Paz Olivera](https://github.com/xLucaspx);
+	- [Nicolas Dutra Tondo](https://github.com/NicolasTondo);
+	- Pedro Gonzatti Chiella;
+	- [Rodrigo Miotto Slongo](https://github.com/Slongo11).
 
 ## TODO
 
 - [x] Criar repositório;
 - [ ] Escolher base de dados;
 - [ ] Implementar funções estatísticas:
-  - [ ] Média aritmética/harmônica/geométrica;
-  - [ ] Mediana;
-  - [ ] Amplitude;
+  - [x] Média aritmética/harmônica/geométrica;
+  - [x] Mediana;
+  - [x] Amplitude;
   - [ ] Variância amostral;
   - [ ] Desvio padrão amostral;
   - [ ] Quartis (Q1, Q2 e Q3);
   - [ ] Outliers (usando IQR - intervalo interquartil).
+- [ ] Testes automatizados;
 - [ ] Documentação;
-- [ ] Testes automatizados comparando com funções nativas;
+- [ ] Comparação com funções nativas;
 - [ ] Relatório;
 - [ ] Slides para apresentação;
+
--- src/main/java/probability/dispersion/Variance.java
+++ src/main/java/probability/dispersion/Variance.java
@@ -1,49 +1,90 @@
 package probability.dispersion;
 
-import probability.central.ArithmeticMean;
 import java.math.BigDecimal;
 import java.util.Arrays;
 
 import static probability.Utils.MATH_CONTEXT;
+import static probability.central.ArithmeticMean.arithmeticMean;
 
 /**
- * Fornece o método {@link #variance(BigDecimal...)} para realizar o
- * cálculo da média aritmética para um conjunto de valores reais.
+ * Fornece os métodos {@link #populationVariance} e {@link #sampleVariance}
+ * ára realizar o cálculo da variância para um conjunto de valores reais.
  *
+ * @author Lucas da Paz
  * @author Rodrigo Miotto Slongo
  */
 public class Variance {
-	private static BigDecimal arithmeticMean;
+
 	/**
-	 * Realiza o cálculo da variância do conjunto de valores passado
-	 * como argumento.
+	 * Realiza o cálculo de variáncia da população para o conjunto de dados
+	 * passado como argumento.
+	 * <p>
+	 * <strong>Fórmula</strong>: &sigma;&sup2; = &sum;(xi - &mu;)&sup2; / N
+	 * <ul>
+	 *   <li><strong>&sigma;&sup2;</strong>: Variância da população;</li>
+	 *   <li><strong>xi</strong>: Cada valor individual no conjunto de dados;</li>
+	 *   <li><strong>&mu;</strong>: Média da população;</li>
+	 *   <li><strong>N</strong>: Número total de observações na população.</li>
+	 * </ul>
 	 *
 	 * @param values Conjunto sobre o qual ocorrerá o cálculo; não pode ser vazio.
-	 * @return Valor correspondente à variância do conjunto.
-	 * @throws ArithmeticException Se o conjunto de dados informado for vazio ou de 1 elemento.
+	 * @return Valor correspondente à variância populacional do conjunto.
+	 * @throws ArithmeticException  Se o conjunto de dados informado for vazio ou unitário.
+	 * @throws NullPointerException Se o conjunto de dados informado for nulo.
 	 */
-	public static BigDecimal variance(BigDecimal... values) {
-		arithmeticMean = ArithmeticMean.arithmeticMean(values);
-
-		BigDecimal resultSomatory = arithmeticMeanSomatory(values);
-		BigDecimal length = BigDecimal.valueOf(values.length -1);
-
-		return resultSomatory.divide(length, MATH_CONTEXT).stripTrailingZeros();
+	public static BigDecimal populationVariance(BigDecimal... values) {
+		BigDecimal n = BigDecimal.valueOf(values.length);
+		return variance(n, values);
 	}
 
 	/**
-	 * Realiza a somatório dos elementos na do conjunto de dados
-	 * - {@link ArithmeticMean#arithmeticMean(BigDecimal...)}; equivalente à
-	 * &sum; {@code (Xi - média aritmética)ˆ2};
+	 * Realiza o cálculo de variáncia da amostra para o conjunto de dados
+	 * passado como argumento.
+	 * <p>
+	 * <strong>Fórmula</strong>: s&sup2; = &sum;(xi - x&#772;)&sup2; / (n - 1)
+	 * <ul>
+	 *   <li><strong>s&sup2;</strong>: Variância da amostra;</li>
+	 *   <li><strong>xi</strong>: Cada valor individual no conjunto de dados;</li>
+	 *   <li><strong>x&#772;</strong>: Média da amostra;</li>
+	 *   <li><strong>n</strong>: Número de observações da amostra.</li>
+	 * </ul>
 	 *
-	 * @param values Conjunto sobre o qual ocorrerá o cálculo.
-	 * @return O resultado da soma do somatório.
+	 * @param values Conjunto sobre o qual ocorrerá o cálculo; não pode ser vazio.
+	 * @return Valor correspondente à variância amostral para o conjunto.
+	 * @throws ArithmeticException  Se o conjunto de dados informado for vazio ou unitário.
+	 * @throws NullPointerException Se o conjunto de dados informado for nulo.
 	 */
-	private static BigDecimal arithmeticMeanSomatory(BigDecimal[] values) {
-		return Arrays.stream(values)
-				.map(e -> e.subtract(arithmeticMean).pow(2))
-				.reduce(BigDecimal.ZERO,BigDecimal::add);
+	public static BigDecimal sampleVariance(BigDecimal... values) {
+		BigDecimal nMinusOne = BigDecimal.valueOf(values.length - 1);
+		return variance(nMinusOne, values);
 	}
 
-}
+	/**
+	 * Realiza o cálculo da variância do conjunto de valores passado como argumento
+	 * utilizando o parâmetro {@code n} informado.
+	 *
+	 * @param n      Valor sobre o qual será dividido o somatório para obter a variância; difere
+	 *               entre as variâncias {@link #sampleVariance amostral} e {@link #populationVariance populacional}.
+	 * @param values Conjunto sobre o qual ocorrerá o cálculo; não pode ser vazio.
+	 * @return Valor correspondente à variância calculada.
+	 */
+	private static BigDecimal variance(BigDecimal n, BigDecimal... values) {
+		BigDecimal arithmeticMean = arithmeticMean(values);
+		BigDecimal somatory = arithmeticMeanSomatory(arithmeticMean, values);
 
+		return somatory.divide(n, MATH_CONTEXT).stripTrailingZeros();
+	}
+
+	/**
+	 * Realiza o somatório utilizado para calcular a variância para o conjunto de dados
+	 * informado: subtrai cada valor pela média aritmética e eleva ao quadrado; o somatório
+	 * é o resultado da soma destes valores.
+	 *
+	 * @param arithmeticMean A média aritmética do conjunto.
+	 * @param values         Conjunto sobre o qual ocorrerá o cálculo.
+	 * @return O resultado do somatório.
+	 */
+	private static BigDecimal arithmeticMeanSomatory(BigDecimal arithmeticMean, BigDecimal... values) {
+		return Arrays.stream(values).map(x -> x.subtract(arithmeticMean).pow(2)).reduce(BigDecimal.ZERO, BigDecimal::add);
+	}
+}
