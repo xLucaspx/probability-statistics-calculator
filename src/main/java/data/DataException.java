0bbd3078a6d1537f3b96792547b6caff5a95532f
Lucas da Paz <xlucaspx@gmail.com>
2025-03-16 12:51:31 -0300

    feat: adiciona base de dados e classes para realizar a manipulação

---
+++ src/main/java/data/DataException.java
@@ -0,0 +1,12 @@
+package data;
+
+public class DataException extends RuntimeException {
+
+	public DataException(String message) {
+		super(message);
+	}
+
+	public DataException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
--- src/test/java/probability/dispersion/VarianceTest.java
+++ src/test/java/probability/dispersion/VarianceTest.java
@@ -27,20 +27,11 @@ public class VarianceTest {
 	 */
 	static Stream<Arguments> varianceTest1Values() {
 		return Stream.of(
-				Arguments.of(toBigDecimalArray(42.0, 2), new BigDecimal("8E2")),
-				Arguments.of(toBigDecimalArray(5.0, 7, 10.0, 12.0, 15), new BigDecimal("15.7")),
-				Arguments.of(toBigDecimalArray(18.0, 21f, 21, 20, 25), new BigDecimal("6.5")),
-				Arguments.of(toBigDecimalArray(12.4, 3.9, 12.125, 23.15, 1.119, 42.21, -4.404, 2.0, 8.0, 1.5), new BigDecimal("187.121066888889"))
+			Arguments.of(toBigDecimalArray(42.0, 2), new BigDecimal("8E2")),
+			Arguments.of(toBigDecimalArray(5.0, 7, 10.0, 12.0, 15), new BigDecimal("15.7")),
+			Arguments.of(toBigDecimalArray(18.0, 21f, 21, 20, 25), new BigDecimal("6.5")),
+			Arguments.of(toBigDecimalArray(12.4, 3.9, 12.125, 23.15, 1.119, 42.21, -4.404, 2.0, 8.0, 1.5), new BigDecimal("187.121066888889"))
 		);
-	}
-
-	@ParameterizedTest
-	@MethodSource("varianceTest1Values")
-	@DisplayName("Deve calcular a variância amostral")
-	void varianceTest1(BigDecimal[] in, BigDecimal expected) {
-		BigDecimal actual = Variance.sampleVariance(in);
-
-		assertEquals(expected, actual);
 	}
 
 	/**
@@ -51,11 +42,20 @@ public class VarianceTest {
 	 */
 	static Stream<Arguments> varianceTest2Values() {
 		return Stream.of(
-				Arguments.of(toBigDecimalArray(42.0, 2), new BigDecimal("4E2")),
-				Arguments.of(toBigDecimalArray(5.0, 7, 10.0, 12.0, 15), new BigDecimal("12.56")),
-				Arguments.of(toBigDecimalArray(18.0, 21f, 21, 20, 25), new BigDecimal("5.2")),
-				Arguments.of(toBigDecimalArray(12.4, 3.9, 12.125, 23.15, 1.119, 42.21, -4.404, 2.0, 8.0, 1.5), new BigDecimal("168.4089602"))
+			Arguments.of(toBigDecimalArray(42.0, 2), new BigDecimal("4E2")),
+			Arguments.of(toBigDecimalArray(5.0, 7, 10.0, 12.0, 15), new BigDecimal("12.56")),
+			Arguments.of(toBigDecimalArray(18.0, 21f, 21, 20, 25), new BigDecimal("5.2")),
+			Arguments.of(toBigDecimalArray(12.4, 3.9, 12.125, 23.15, 1.119, 42.21, -4.404, 2.0, 8.0, 1.5), new BigDecimal("168.4089602"))
 		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("varianceTest1Values")
+	@DisplayName("Deve calcular a variância amostral")
+	void varianceTest1(BigDecimal[] in, BigDecimal expected) {
+		BigDecimal actual = Variance.sampleVariance(in);
+
+		assertEquals(expected, actual);
 	}
 
 	@ParameterizedTest
@@ -79,7 +79,7 @@ public class VarianceTest {
 	@Test
 	@DisplayName("Deve lançar exceção para array unitário na variância amostral")
 	void varianceTest4() {
-		BigDecimal[] in = new BigDecimal[]{ new BigDecimal("1") };
+		BigDecimal[] in = new BigDecimal[]{new BigDecimal("1")};
 
 		assertThrows(ArithmeticException.class, () -> Variance.sampleVariance(in));
 	}
